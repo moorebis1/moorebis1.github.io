9f7b6905922e29c3454490412e36e2bd53d75c01
moorebis <moorebisaac@gmail.com>
2023-01-18 08:37:04 -0800

    Projects

---
+++ My Projects/Volume of a Box Calculator.cpp
@@ -0,0 +1,26 @@
+//-----------------------//
+// APSC 177 Assignment 1 //
+//   January 17th, 2023  //
+//      Isaac Moore      //
+//       #94333416       //
+//    Volume Calculator  //
+//-----------------------//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int width = 15; //defines the value of the width variable
+    int depth = 10; //defines the value of the depth variable
+    double height = 5.5; //defines the value of the height variable
+
+    cout << "width of box = " << width << " cm" << endl; //states the box width
+    cout << "depth of box = " << depth << " cm" << endl; //states the box depth
+    cout << "height of box = " << height << " cm" << endl; //states the box height
+    cout << "volume of box = " << width*depth*height << " cm^3" << endl; //computes for the boxes volume
+
+    return 0; //responds to the computer to "tell" it that everything was successful
+
+}
